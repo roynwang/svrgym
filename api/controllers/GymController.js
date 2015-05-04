328aa0e084badfd3a3bf4cf1c907fa889838063a
roynwang <royn.wang.renyuan@gmail.com>
2015-05-04 23:47:50 +0800

    use request param as lbs query data

--- api/controllers/GymController.js
+++ api/controllers/GymController.js
@@ -8,34 +8,32 @@
 
 module.exports = {
 
-  /**
-   * `GymsvcController.nearby()`
-   */
-  nearby: function (req, res) {
-	var lon = 116.431893;
-	var lat = 39.992704
-	var cb = function(ht){
-		var nearby = JSON.parse(ht);
-		if(nearby.status != 0){
-			//error handler
-		}
-		else{
-			var gymlist = [];
-			nearby.contents.forEach(function(item){
-				var gymitem = {
-					width: 60,
-					height: 49,
-				};
-				gymitem['longitude'] = item.location[0];
-				gymitem['latitude'] = item.location[1];
-				gymitem['title'] = item.title;
-				gymitem['content'] = "renyuantest";
-				gymlist.push(gymitem);
-			});
-			res.json(gymlist);
-		}
-	};
-	GeoSvc.getnearby(lon, lat, cb, cb);
-  }
+    /**
+     * `GymsvcController.nearby()`
+     */
+    nearby: function(req, res) {
+        var lon = req.param('longitude');
+        var lat = req.param('latitude');
+        var cb = function(ht) {
+            var nearby = JSON.parse(ht);
+            if (nearby.status != 0) {
+                //error handler
+            } else {
+                var gymlist = [];
+                nearby.contents.forEach(function(item) {
+                    var gymitem = {
+                        width: 60,
+                        height: 49,
+                    };
+                    gymitem['longitude'] = item.location[0];
+                    gymitem['latitude'] = item.location[1];
+                    gymitem['title'] = item.title;
+                    gymitem['content'] = "renyuantest";
+                    gymlist.push(gymitem);
+                });
+                res.json(gymlist);
+            }
+        };
+        GeoSvc.getnearby(lon, lat, cb, cb);
+    }
 };
-
